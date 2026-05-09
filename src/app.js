34a9db359caefb7f06911731bfd48290b129d230
mahirsiam2004 <mahirsiam2004@gmail.com>
2026-05-09 21:36:37 +0600

    added data in books

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
-const express = require("express")
-const app= express();
+const express = require("express");
+const cors = require("cors");
+const bookRoutes = require("./routes/Bookroutes");
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-module.exports=app;
+app.use("/api/books", bookRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Bookstore API is running" });
+});
+
+module.exports = app;
--- src/routes/Bookroutes.js
+++ src/routes/Bookroutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/",async (req,res)=>{
     try{
         const books = await Book.find();
-        req.json(books);
+        res.json(books);
     }
     catch(error){
        res.status(500).json({ message: error.message });
