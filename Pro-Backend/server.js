9cc6b7b66990e6391743424c57cddb19888843a9
mahirsiam2004 <mahirsiam2004@gmail.com>
2026-07-07 20:08:29 +0600

    hidded .env by accident

---
+++ Pro-Backend/server.js
@@ -0,0 +1,9 @@
+const app = require("./src/app")
+const connectDB=require("./src/db/db")
+const dotenv =require('dotenv').config();
+
+connectDB();
+
+app.listen(3000,()=>[
+    console.log("server is running on 3000")
+])
