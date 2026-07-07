2b71f6b376a697715f6035870c1b4e74ace542eb
mahirsiam2004 <mahirsiam2004@gmail.com>
2026-07-07 21:46:14 +0600

    added all boobs routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,60 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.listen(port,()=>{
-    console.log(`server is running on ${port}`)
-})
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const db = client.db("aperture");
+    const booksCollection = db.collection("Books");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    app.get("/books", async (req, res) => {
+      const result = await booksCollection.find().toArray();
+      res.send(result);
+    });
+
+  } 
+  
+  
+  
+  
+  finally {
+
+
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("You server api is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
